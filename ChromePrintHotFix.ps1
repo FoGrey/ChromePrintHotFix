3dfddbc4e7f9726b75e94edd6c785e8d6b8a361f
Forest Burchinal <forestburchinal13@gmail.com>
2021-02-23 11:17:15 -0700

    Added .gitignore

--- ChromePrintHotFix.ps1
+++ ChromePrintHotFix.ps1
@@ -1,12 +1,6 @@
 #Closes Chrome.
 Get-Process chrome | stop-process
 
-#Removes printers on old servers.
-Remove-Printer \\kwsprintsrvr\*
-Remove-Printer \\calsrv\*
-Remove-Printer \\ontsrv\*
-Remove-Printer \\heysrv\*
-
 #Edits Chrome JSON preferences file removing recent print destinations. All other User Chrome preferences alone.
 $path = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Preferences"
 $readableJSON = Get-Content -Path $path | ConvertFrom-Json -ErrorAction Stop
@@ -16,5 +10,3 @@ New-Item -ItemType file -Path $path -Force
 $readableJSON | ConvertTo-Json -depth 99 -Compress | Add-Content -Path $path
 }
 
-#Reopens Chrome.
-Start-Process -FilePath "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
